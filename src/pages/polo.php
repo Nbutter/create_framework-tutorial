ca3d7ffb7f6d44847beda54b82b530420ad19ffb
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-09-27 14:01:56 -0400

    played around with polo.php

--- src/pages/polo.php
+++ src/pages/polo.php
@@ -1,3 +1,12 @@
-<!-- this should be an html template too, right? -->
+<!-- this is an html template -->
 
 <h1>Polo!</h1>
+
+<!-- just checking that this works -->
+<p>You requested this path: <?php echo $request->getPathInfo() ?> </p>
+
+<p>The file which executed to bring you this message (ie $_SERVER['SCRIPT_FILENAME']) is: <?php echo $_SERVER['SCRIPT_FILENAME'] ?>.</p>  
+
+<p>Naturally, exposing this much system information constitutes a security risk and we'd never actually do anything like this in the wild.</p>
+
+
